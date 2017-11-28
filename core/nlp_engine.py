118f1bf6fe231f729b1614d1c3ce7a941221d935
emanuele luzio <emanueleluzio@emanueles-MacBook-Pro.local>
2017-11-28 08:16:18 -0300

    new method for nlp engine

--- core/nlp_engine.py
+++ core/nlp_engine.py
@@ -1,8 +1,19 @@
 class NLPEngine(object):
+    def __init__(self):
+        pass
+
     def echo(self, message):
         """
-        Echo method return what it get.
+        Simple method for testing purpose. Echo method return what it get.
         :param message: string
         :return: string message
         """
         return "You said: {}".format(message)
+
+    def think(self, message):
+        """
+        This method is in charge to run all the nlp pipe
+        :param message: string
+        :return: string
+        """
+        return "Hello World!"
